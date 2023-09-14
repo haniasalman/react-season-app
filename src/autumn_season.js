1ffe0f68701209e59b3a8c6bb57cf04e5cdd7adc
haniasalman <hania.salman99@gmail.com>
2023-09-14 11:44:08 +0500

    getseason function added

--- src/autumn_season.js
+++ src/autumn_season.js
@@ -1,1 +1,8 @@
 console.log('This is autumn season');
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'autumn' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'autumn';
+  }
+};
